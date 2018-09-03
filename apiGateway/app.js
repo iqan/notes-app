9b183a71648cf5760be249186dde611fefec2dc7
iqans <iqan.shaikh@cognizant.com>
2018-09-03 16:07:48 +0530

    fixed docker-compose

--- apiGateway/app.js
+++ apiGateway/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const httpProxy = require('http-proxy-middleware');
 const config = require('./config');
+const cors = require('cors');
 
 let app = express();
 
 // Configure morgan to log your requests, with a standard date & time format
 morgan.token('time', (req, res) => new Date().toISOString());
 app.use(morgan('[:time] :remote-addr :method :url :status :res[content-length] :response-time ms'));
+app.use(cors());
 
 app.use('/healthcheck', (req, res) => {
 	res.send("It's running...!");
