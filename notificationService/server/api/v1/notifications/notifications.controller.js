03fc404904bddd751acc869b20175f108835f9e7
Iqan Shaikh <Iqan.Shaikh@asos.com>
2018-09-21 16:17:36 +0100

    updated notification service as per feeback and error handling improved

--- notificationService/server/api/v1/notifications/notifications.controller.js
+++ notificationService/server/api/v1/notifications/notifications.controller.js
@@ -3,29 +3,39 @@ const notificationsService = require('./notifications.service');
 const notifyUser = (req, res) => {
   const userId = req.userData.userId;
   const notificationWithNotes = req.body;
-  notificationsService.notifyUser(res, userId, notificationWithNotes);
+  notificationsService.notifyUser(userId, notificationWithNotes)
+    .then(result => res.status(result.status).json(result))
+    .catch(err => res.status(err.status).json(err));
 }
 
 const getReminders = (req, res) => {
   const userId = req.userData.userId;
-  notificationsService.getReminders(res, userId);
+  notificationsService.getReminders(userId)
+    .then(result => res.status(result.status).json(result.notifications))
+    .catch(err => res.status(err.status).json(err));
 }
 
 const addReminder = (req, res) => {
   const userId = req.userData.userId;
   const notification = req.body;
-  notificationsService.addReminder(res, userId, notification);
+  notificationsService.addReminder(userId, notification)
+    .then(result => res.status(result.status).json(result))
+    .catch(err => res.status(err.status).json(err));
 }
 
 const snoozeReminder = (req, res) => {
   const notificationId = req.params.reminderId;
   const notification = req.body;
-  notificationsService.snoozeReminder(res, notificationId, notification);
+  notificationsService.snoozeReminder(notificationId, notification)
+    .then(result => res.status(result.status).json(result))
+    .catch(err => res.status(err.status).json(err));
 }
 
 const dismissReminder = (req, res) => {
   const notificationId = req.params.reminderId;
-  notificationsService.dismissReminder(res, notificationId);
+  notificationsService.dismissReminder(notificationId)
+    .then(result => res.status(result.status).json(result))
+    .catch(err => res.status(err.status).json(err));
 }
 
 module.exports = {
